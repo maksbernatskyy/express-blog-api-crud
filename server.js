c2f0e932defa503748a1b03bd5649afe6f02ad30
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-26 11:29:42 +0100

    fix the package.json

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+
+
+app.get('/', (req, res) => {
+    res.send('Server del mio blog')
+})
+
+app.listen(PORT, () => {
+    console.log(`app listening on port: ${PORT}`)
+})
+
