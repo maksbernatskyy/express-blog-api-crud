8152a5f9d9c4b14ea45391e245c18472e0fff358
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-26 12:24:02 +0100

    add filter in index

--- routers/posts.js
+++ routers/posts.js
@@ -6,7 +6,24 @@ const blog = require('../data/postsArr')
 
 // index
 router.get('/', (req, res) => {
-    res.json(blog)
+    // Now filtered blog is equal to the original blog
+    let filteredBlog = blog
+
+    // Verify if there is a filter
+    if(req.query.tag) {
+        filteredBlog = blog.filter((thisPost) => {
+            return thisPost.tags.includes(req.query.tag)
+        })
+        console.log(filteredBlog)
+    } 
+
+    // Response with the filterd menu
+    res.json(filteredBlog)
+})
+
+// show
+router.get('/:id', (req, res) => {
+
 })
 
 module.exports = router
