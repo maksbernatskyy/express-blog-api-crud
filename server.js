d2960c34a58aea2bc25d0f5b33b08efaa7495fca
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-28 11:53:40 +0100

    add notFound

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
-const express = require('express')
-const app = express()
-const PORT = 3000
-const postsRouter = require('./routers/posts')
-const errorsHandler = require('./middlewares/errorsHandler')
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const postsRouter = require("./routers/posts");
+const errorsHandler = require("./middlewares/errorsHandler");
+const notFound = require("./middlewares/notFound");
 
 // Access to folder public
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.use(express.json())
+app.use(express.json());
 
 app.post("/", (req, res) => {
-    console.log(req.body)
-})
+  console.log(req.body);
+});
 
-app.get('/', (req, res) => {
-    res.send('Server del mio blog')
-})
+app.get("/", (req, res) => {
+  res.send("Server del mio blog");
+});
 
 // Use posts router
-app.use('/posts', postsRouter)
+app.use("/posts", postsRouter);
 
 // View in console the port
 app.listen(PORT, () => {
-    console.log(`app listening on port: ${PORT}`)
-})
+  console.log(`app listening on port: ${PORT}`);
+});
 
-app.use(errorsHandler)
+app.use(errorsHandler);
 
+app.use(notFound);
